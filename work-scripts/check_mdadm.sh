b0611a4db41f7f2fa24378ea54483e64315c7314
darkhex <root.murashov@gmail.com>
2015-01-30 11:23:43 +0300

    change raid

--- work-scripts/check_mdadm.sh
+++ work-scripts/check_mdadm.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
-# Script to check software raid status and email if there is a problem
-# Written by Garth van Sittert
 
-# Some variables to specify
-email="me@me.com"
+email="test@mail.ru"
 
+cat /proc/mdstat | grep active > /dev/null
+if [ $? -ne 0 ]; then
+	    mail -s "RAID failure at `hostname`" "$email" << EOF
 
+	    ===================================================
 
-##########################################################
+	    `cat /proc/mdstat`
 
-# Display RAID status
-cat /proc/mdstat
-
-# Check for [U_] or [_U]
-if ! egrep "\[.*_.*\]" /proc/mdstat  > /dev/null
-then exit
-fi
-
-# email a report
-mail -s "RAID failure at `hostname`" "$email" << EOF
-
-===================================================
-
-`cat /proc/mdstat`
-
-===================================================
-EOF
+	    ===================================================
+	    EOF
+    fi
