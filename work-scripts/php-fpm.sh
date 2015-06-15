6c617e29b6eaba8e02c7a7d079a4a64eea01ecc8
darkhex <root.murashov@gmail.com>
2015-06-15 15:06:42 +0300

    add check php-fpm

--- work-scripts/php-fpm.sh
+++ work-scripts/php-fpm.sh
@@ -1,34 +1,31 @@
 #!/bin/bash    
-#description    : check nginx_status    
+#description    : check php-fpm  
 #author         :darkhex
 #version        :0.1
-#usage          :./ngninx.sh
+#usage          :./check_php.sh
 #notes          :       
 #============================================================================
+output=/tmp/out
 email="root.murashov@gmail.com"
 server=`ip a | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | grep -v 127.0.0.1 | sort -u`
 host=`hostname`
 
+ps aux | grep php | grep -v grep  | awk '{print $1}' > $output
 
-Failxen()
+RestartPHP()
 {
- 	mail -s "Fail xen on $server" "$email" << EOF
+	systemctl restart php-fpm.service 
+ 	mail -s "RestartPHP-FPM " "$email" << EOF
     Server: $server$
     Hostname: $host
 	===================================================
-	`systemctl status nginx.service`
+	`systemctl status php-fpm.service `
 	===================================================
 EOF
 }
 
-#xl top | tr '\r' '\n' | sed 's/[0-9][;][0-9][0-9][a-Z]/ /g' | col -bx | sed 1,4d | awk '{print $2,$5,$6}'
-
-xm list | awk '{print $5}' | grep -v State
-
-if grep -q r,b 
-	then echo "All ok" > /dev/null
+if egrep  "apache| root" $output; then
+        echo "OK" > /dev/null
 else
-   Failxen
+    	systemctl restart php-fpm.service 
 fi
-
-
